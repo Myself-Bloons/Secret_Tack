40e74d98bb8042e45c8a5d7bfa112fe62df43ec4
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-10-29 14:20:15 +0100

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace SecretTack;
+
+public static class ModHelperData
+{
+    public const string Name = "The Secret Tack";
+    public const string Description = "Bullet hell-style custom tower that shoots projectiles in 360-degree patterns.";
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "Myself-Bloons";
+    public const string RepoName = "The-Secret-Tack";
+    public const string Author = "Myself";
+    public const string WorksOnVersion = "51.1";
+    public const string DllName = "SecretTack.dll";
+    public const string Icon = "Icon.png";
+}
