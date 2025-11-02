3340587a5b3c0250df9a029b0c133e51d2b9554b
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-11-02 20:43:50 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,7 @@ public static class ModHelperData
 {
     public const string Name = "The Secret Tack";
     public const string Description = "Bullet hell-style custom tower that shoots projectiles in 360-degree patterns.";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.1";
     public const string RepoOwner = "Myself-Bloons";
     public const string RepoName = "The-Secret-Tack";
     public const string Author = "Myself";
